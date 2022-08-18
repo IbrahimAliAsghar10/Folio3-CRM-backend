a3c5cc66ffa33b9cef4010c029ea5dc835220070
IbrahimAliAsghar10 <ibrahimaliasghar10@gmail.com>
2022-08-18 18:04:54 +0500

    app.listen changes

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT||5000;
+  const port = process.env.PORT || 5000;
   app.enableCors();
-  await app.listen(port);
+  await app.listen(port,() =>{
+    console.log(`Server running on port ${port}`)
+  });
 }
 bootstrap();
